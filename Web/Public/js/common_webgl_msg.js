b65746e81a2217b4e4d29f675e83a4c489181507
平 <37074843+Cutepingping@users.noreply.github.com>
2018-09-04 14:52:12 +0800

    Add files via upload

---
+++ Web/Public/js/common_webgl_msg.js
@@ -0,0 +1,34 @@
+/*-----------------------------------------------------------------
+该文件为webgl页面发送消息事件给web页面的事件和方法
+在web页面和webgl页面交互过程中，由于方法事件较多   
+以下标签 或 方法名称的定义 统一规范命名如：
+  某某页面_消息_某某页面_做什么事情_怎么做（用什么来做或者说是传什么参数）
+  消息名称的定义：
+  传递--  transmit
+  通知--  call
+  获取--  get
+  提交--  post
+  例子：
+  function webgl_call_web_show_or_hide_left_menu(){}  解释：这个方法的意思是 webgl页面通知web页面显示或隐藏左菜单*/
+jQuery(function($){
+	
+})
+
+//webgl通知web页面关闭视频
+// function webgl_call_web_close_video() {	
+    // playPause();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
